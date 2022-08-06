7682896497664849b9ec104daa5fc83e13f49689
romofel <81869916+romofel@users.noreply.github.com>
2022-08-06 21:16:07 +0000

    Change file extension to jsx

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+
+function Nav() {
+  return <nav></nav>
+}
+
+function App() {
+  return (
+    <div css={css`
+  font-family: 'Epilogue', sans-serif;
+    `}>App Component</div>
+  );
+}
+
+export default App;
