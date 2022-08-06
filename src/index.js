8b2e78df22c9220ee0a86190b9c4b3782d956f9d
romofel <81869916+romofel@users.noreply.github.com>
2022-08-06 20:43:27 +0000

    Reset the global css styles

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { css, Global } from '@emotion/react';
+import emotionReset from 'emotion-reset';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Global styles={css`${emotionReset}`} />
     <App />
   </React.StrictMode>
 );
