32b89488001051b4468967663c45ed31f39ef521
romofel <81869916+romofel@users.noreply.github.com>
2022-08-06 21:21:06 +0000

    Add skeleton for landing page mobile version

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,34 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
 
+function LandingPage() {
+  return (
+    <>
+      <Nav />
+      <div>Hero Image</div>
+      <h1>Make remote work</h1>
+      <p>Get your team in sync, no matter your location. Streamline processes, create team rituals, and watch productivity soar.</p>
+      <button>Learn more</button>
+      <ul className="affiliates">
+        <li>databiz</li>
+        <li>audiophile</li>
+        <li>meet</li>
+        <li>maker</li>
+      </ul>
+    </>
+  );
+}
+
 function Nav() {
-  return <nav></nav>
+  return <nav>
+    <h2>snap</h2>
+    <div className="hamburger-menu"></div>
+  </nav>
 }
 
 function App() {
   return (
-    <div css={css`
-  font-family: 'Epilogue', sans-serif;
-    `}>App Component</div>
+    <LandingPage />
   );
 }
 
