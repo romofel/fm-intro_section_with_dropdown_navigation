d95e514a5f0469bf5d90a8bfaf097196cdc11860
romofel <81869916+romofel@users.noreply.github.com>
2022-08-07 10:05:05 +0000

    Style the Affiliates and Affiliate components

--- src/Affiliates.jsx
+++ src/Affiliates.jsx
@@ -6,14 +6,27 @@ import clientDatabiz from './assets/client-databiz.svg';
 import clientMaker from './assets/client-maker.svg';
 import clientMeet from './assets/client-meet.svg';
 
+const affiliateStyles = css`
+& img {
+  height: 16px;
+`;
+
 function Affiliate({ src, alt }) {
-  return <li>
+  return <li css={affiliateStyles}>
     <img src={src} alt={alt} />
   </li>
 }
 
+const affiliatesStyles = css`
+display: flex;
+justify-content: space-between;
+padding-left: 1rem;
+padding-right: 1rem;
+margin-top: 3rem;
+`;
+
 export default function Affiliates() {
-  return <ul>
+  return <ul css={affiliatesStyles}>
     {
       [clientDatabiz, clientAudiophile, clientMeet, clientMaker].map(client => <Affiliate src={client} alt="affiliate client logo" />)
     }
