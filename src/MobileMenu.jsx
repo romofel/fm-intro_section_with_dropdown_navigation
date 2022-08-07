a54d84cf2545fade80ac271f22a7f100dbe11d7c
romofel <81869916+romofel@users.noreply.github.com>
2022-08-07 12:02:05 +0000

    Style the background size and position

--- src/MobileMenu.jsx
+++ src/MobileMenu.jsx
@@ -4,6 +4,10 @@ import { css } from '@emotion/react';
 
 const menuStyles = css`
 position: absolute;
+right: 0;
+background-color: white;
+height: 100vh;
+width: 65%;
 `;
 
 export default function MobileMenu() {
