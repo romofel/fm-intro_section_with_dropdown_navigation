6b3cfd38aee075b7fa287c07b0d38ada90cf8507
romofel <81869916+romofel@users.noreply.github.com>
2022-08-07 09:18:41 +0000

    Style the typography and spacing for Nav

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,9 +1,20 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+
+const navStyles = css`
+color: black;
+font-family: 'Epilogue', sans-serif;
+font-weight: 700;
+font-size: 35px;
+padding: 1rem;
+`;
+
 export default function Nav() {
   return (
-    <nav>
-      <h2>snap</h2>
-      <div className="hamburger-menu"></div>
-    </nav>
+  <nav css={navStyles}>
+    <h2>snap</h2>
+    <div className="hamburger-menu"></div>
+  </nav>
   );
 
 }
