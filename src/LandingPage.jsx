9b8e72e1866914f636b6a0152ee480a2a62a177f
romofel <81869916+romofel@users.noreply.github.com>
2022-08-07 09:23:39 +0000

    Display the hero image for mobile

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -1,10 +1,13 @@
 import Nav from './Nav';
+import mobileHero from './assets/image-hero-mobile.png'
 
 export default function LandingPage() {
   return (
     <>
       <Nav />
-      <div>Hero Image</div>
+      <div>
+      <img src={mobileHero} alt="Hero for mobile" />
+      </div>
       <h1>Make remote work</h1>
       <p>Get your team in sync, no matter your location. Streamline processes, create team rituals, and watch productivity soar.</p>
       <button>Learn more</button>
