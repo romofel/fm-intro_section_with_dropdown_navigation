03f3dfa59b7e3233c54e1bb96a38d6bebba1088a
romofel <81869916+romofel@users.noreply.github.com>
2022-08-07 09:53:04 +0000

    Refactor to use newly created Affiliate component

--- src/Affiliates.jsx
+++ src/Affiliates.jsx
@@ -6,8 +6,14 @@ import clientDatabiz from './assets/client-databiz.svg';
 import clientMaker from './assets/client-maker.svg';
 import clientMeet from './assets/client-meet.svg';
 
+function Affiliate({ src, alt }) {
+  return <li>
+    <img src={src} alt={alt} />
+  </li>
+}
+
 export default function Affiliates() {
   return <ul>
-    <li><img src={clientDatabiz} alt="databiz client logo" /></li>
+    <Affiliate src={clientDatabiz} alt="databiz client logo" />
   </ul>
 }
