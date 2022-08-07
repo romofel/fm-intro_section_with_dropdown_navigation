6596d5b839b8ba2a1daafac116919a48047d462d
romofel <81869916+romofel@users.noreply.github.com>
2022-08-07 09:50:27 +0000

    Import all the client logos and display one for testing

--- src/Affiliates.jsx
+++ src/Affiliates.jsx
@@ -1,7 +1,13 @@
 /** @jsxImportSource @emotion/react */
 
 import { css } from '@emotion/react';
+import clientAudiophile from './assets/client-audiophile.svg';
+import clientDatabiz from './assets/client-databiz.svg';
+import clientMaker from './assets/client-maker.svg';
+import clientMeet from './assets/client-meet.svg';
 
 export default function Affiliates() {
-  return <div>Affiliates Component</div>
+  return <ul>
+    <li><img src={clientDatabiz} alt="databiz client logo" /></li>
+  </ul>
 }
