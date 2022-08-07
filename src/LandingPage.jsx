2cf5d08418a77a9bde7e3f94c545ddfada2153bd
romofel <81869916+romofel@users.noreply.github.com>
2022-08-07 09:27:40 +0000

    Style heading typography

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -9,14 +9,22 @@ const heroStyles = css`
 width: 100%;
 `;
 
+const headingStyles = css`
+color: black;
+font-family: 'Epilogue', sans-serif;
+font-weight: bold;
+font-size: 2.25rem;
+text-align: center;
+`;
+
 export default function LandingPage() {
   return (
     <>
       <Nav />
       <div>
-      <img css={heroStyles} src={mobileHero} alt="Hero for mobile" />
+        <img css={heroStyles} src={mobileHero} alt="Hero for mobile" />
       </div>
-      <h1>Make remote work</h1>
+      <h1 css={headingStyles}>Make remote work</h1>
       <p>Get your team in sync, no matter your location. Streamline processes, create team rituals, and watch productivity soar.</p>
       <button>Learn more</button>
       <ul className="affiliates">
