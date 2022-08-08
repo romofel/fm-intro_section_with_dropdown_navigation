715c697a0c98dd0eda64aa0f9a615e9689cb5119
Felipe Romo <felipe@romofel.dev>
2022-08-07 19:20:08 -0500

    Formatting

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,24 +1,31 @@
 /** @jsxImportSource @emotion/react */
-import { css } from '@emotion/react';
-import Hamburger from 'hamburger-react';
+import { css } from "@emotion/react";
+import Hamburger from "hamburger-react";
 
 const navStyles = css`
-color: black;
-display: flex;
-justify-content: space-between;
-align-items: center;
-font-family: 'Epilogue', sans-serif;
-font-weight: 700;
-font-size: 35px;
-padding: 1rem;
+  color: black;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-family: "Epilogue", sans-serif;
+  font-weight: 700;
+  font-size: 35px;
+  padding: 1rem;
+
+  @media (min-width: 1024px) {
+    .mobile-menu {
+      display: none;
+    }
+  }
 `;
 
 export default function Nav({ onToggle }) {
   return (
     <nav css={navStyles}>
       <h2>snap</h2>
-      <Hamburger onToggle={toggled => onToggle(toggled)}/>
+      <div className="mobile-menu">
+        <Hamburger onToggle={(toggled) => onToggle(toggled)} />
+      </div>
     </nav>
   );
-
 }
